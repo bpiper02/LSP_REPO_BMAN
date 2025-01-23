40937a98b1697020ee0b9a7c6ea3194aeac998a6
bpiper02 <93992231+bpiper02@users.noreply.github.com>
2025-01-23 17:40:03 -0500

    Final Push Commiy

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+package org.howard.edu.lsp.assignment1;
+
+
+
+public class HelloWorld {
+	
+	/**
+	 * Brent Piper
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!!!!") ;
+
+	}
+
+}
